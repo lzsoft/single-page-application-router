c4ee530f9512794c7122a2e7e720fa3899de3a9e
JingAn Chen <lzsoft.cja@gmail.com>
2017-01-30 11:45:35 -0500

    Activate the popstate event when all documents are loaded.
    Otherwise some page custom elements may still be undefined, because of
    the <link> loading order.

--- single-page-application-router.js
+++ single-page-application-router.js
@@ -3,8 +3,8 @@ window.customElements.define('single-page-application-router', class extends HTM
         super();
         const map = {};
         // Suppress <a> element
-        let observer = new MutationObserver(function (mutations) {
-            document.querySelectorAll("a:not([data-suppressed])").setAttribute("data-suppressed", true).addEventListener("click", function (e) {
+        let observer = new MutationObserver(function(mutations) {
+            document.querySelectorAll("a:not([data-suppressed])").setAttribute("data-suppressed", true).addEventListener("click", function(e) {
                 e.preventDefault();
                 window.history.pushState(null, null, this.href);
                 window.dispatchEvent(new Event("popstate"));
@@ -17,7 +17,7 @@ window.customElements.define('single-page-application-router', class extends HTM
             subtree: true
         });
         // Construct map
-        this.querySelectorAll(":scope > single-page-application-route").exec(function (el) {
+        this.querySelectorAll(":scope > single-page-application-route").exec(function(el) {
             map[el.getAttribute("data-pattern")] = el.getAttribute("data-element");
         });
         // Deal with popstate event
@@ -34,9 +34,13 @@ window.customElements.define('single-page-application-router', class extends HTM
             let c = window.customElements.get(t);
             if (c) {
                 this.appendChild(new c());
+            } else {
+                console.log("Element not defined");
             }
         });
-        window.dispatchEvent(new Event("popstate"));
+        window.addEventListener("load", (e) => {
+            window.dispatchEvent(new Event("popstate"));
+        });
     }
 });
 window.customElements.define('single-page-application-route', class extends HTMLElement {
