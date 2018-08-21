1edf413b49bd056df2b0be00e9fb21eae2d8013f
JingAn Chen <lzsoft.cja@gmail.com>
2018-08-21 16:31:14 -0400

    Get rid of the root scope and switch to ES6 Module.

--- single-page-application-router.js
+++ single-page-application-router.js
@@ -1,72 +1,71 @@
-{
-	const ATTR_MULTI_PAGE_MODE = "data-multi-page-mode";
-	const ROUTER_MAP = new Map();
-	let previousPathName;
-	window.customElements.define('single-page-application-route', class extends HTMLElement {
-		constructor() {
-			super();
-		}
-	});
-	window.customElements.define('single-page-application-router', class extends HTMLElement {
-		constructor() {
-			super();
-		}
-		async connectedCallback() {
-			if (this.hasAttribute(ATTR_MULTI_PAGE_MODE)) {
-				// Do nothing if we're running in classic multi page mode
-			} else {
-				const observer = new MutationObserver(function(mutations) {
-					for (let a of document.querySelectorAll("a:not([data-suppressed])")) {
-						a.setAttribute("data-suppressed", "");
-						a.addEventListener("click", function(e) {
-							e.preventDefault();
-							window.history.pushState(true, null, this.href);
-							window.dispatchEvent(new Event("popstate"));
-						});
-					}
-				});
-				observer.observe(document, {
-					attributes: false,
-					childList: true,
-					characterData: false,
-					subtree: true
-				});
-				window.addEventListener("popstate", e => {
-					this.navigate();
-				});
-			}
-			// Construct map
-			for (let r of this.querySelectorAll("single-page-application-route")) {
-				ROUTER_MAP.set(r.getAttribute("data-pattern"), r.getAttribute("data-element"));
-			}
-			// Deal with popstate event
-			await this.navigate();
-		}
-		async navigate() {
-			if (previousPathName === window.location.pathname) {
-				return true; // Most likely a hashchange
-			}
-			previousPathName = window.location.pathname;
-			let tt = parseFloat(window.getComputedStyle(this).getPropertyValue('--single-page-application-router-transition-time').replace('s', '')) * 1000;
-			this.classList.add("fade");
-			await new Promise(r => setTimeout(r, tt));
-			this.classList.add("disk");
-			this.classList.remove("fade");
-			let z = this.offsetTop;
-			window.scroll(0, 0);
-			this.innerHTML = "";
-			let p = window.location.pathname;
-			let t = "";
-			let keys = ROUTER_MAP.keys();
-			for (let k of keys) {
-				if (p.indexOf(k) >= 0) {
-					t = ROUTER_MAP.get(k);
+const ATTR_MULTI_PAGE_MODE = "data-multi-page-mode";
+const ROUTER_MAP = new Map();
+let previousPathName;
+window.customElements.define('single-page-application-route', class extends HTMLElement {
+	constructor() {
+		super();
+	}
+});
+window.customElements.define('single-page-application-router', class extends HTMLElement {
+	constructor() {
+		super();
+	}
+	async connectedCallback() {
+		if (this.hasAttribute(ATTR_MULTI_PAGE_MODE)) {
+			// Do nothing if we're running in classic multi page mode
+		} else {
+			const observer = new MutationObserver(function(mutations) {
+				for (let a of document.querySelectorAll("a:not([data-suppressed])")) {
+					a.setAttribute("data-suppressed", "");
+					a.addEventListener("click", function(e) {
+						e.preventDefault();
+						window.history.pushState(true, null, this.href);
+						window.dispatchEvent(new Event("popstate"));
+					});
 				}
-			}
-			customElements.whenDefined(t).then(() => {
-				this.appendChild(new(customElements.get(t)));
 			});
-			this.classList.remove("disk");
+			observer.observe(document, {
+				attributes: false,
+				childList: true,
+				characterData: false,
+				subtree: true
+			});
+			window.addEventListener("popstate", e => {
+				this.navigate();
+			});
 		}
-	});
-}
+		// Construct map
+		for (let r of this.querySelectorAll("single-page-application-route")) {
+			ROUTER_MAP.set(r.getAttribute("data-pattern"), r.getAttribute("data-element"));
+		}
+		// Deal with popstate event
+		await this.navigate();
+	}
+	async navigate() {
+		if (previousPathName === window.location.pathname) {
+			return true; // Most likely a hashchange
+		}
+		previousPathName = window.location.pathname;
+		let tt = parseFloat(window.getComputedStyle(this).getPropertyValue('--single-page-application-router-transition-time').replace('s', '')) * 1000;
+		this.classList.add("fade");
+		await new Promise(r => setTimeout(r, tt));
+		this.classList.add("disk");
+		this.classList.remove("fade");
+		let z = this.offsetTop;
+		window.scroll(0, 0);
+		this.innerHTML = "";
+		let p = window.location.pathname;
+		let t = "";
+		let keys = ROUTER_MAP.keys();
+		for (let k of keys) {
+			if (p.indexOf(k) >= 0) {
+				t = ROUTER_MAP.get(k);
+			}
+		}
+		customElements.whenDefined(t).then(() => {
+			this.appendChild(new(customElements.get(t)));
+		});
+		this.classList.remove("disk");
+	}
+});
+export {};
